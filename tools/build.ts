9fe8631dc437f8c779f8b9bcb77958eaf712fda1
ericchase <github@eric.addymail.com>
2025-03-10 05:51:04 -0500

    (wip)
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -2,7 +2,7 @@ import { Builder } from 'tools/lib/Builder.js';
 import { BuildStep_BunInstall } from 'tools/lib/steps/Bun-Install.js';
 import { BuildStep_IOFormat } from 'tools/lib/steps/IO-Format.js';
 
-const builder = new Builder();
+const builder = new Builder(Bun.argv[2] === '--watch' ? 'watch' : 'build');
 
 builder.setStartupSteps([
   BuildStep_BunInstall(),
