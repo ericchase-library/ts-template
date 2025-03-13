143e1367224a1c0f9fc255633a3e0e942cf42a16
ericchase <github@eric.addymail.com>
2025-03-13 00:07:27 -0500

    more caching, more quiet, better names
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -1,12 +1,12 @@
 import { Builder } from 'tools/lib/Builder.js';
 import { BuildStep_BunInstall } from 'tools/lib/steps/Bun-Install.js';
-import { BuildStep_IOFormat } from 'tools/lib/steps/IO-Format.js';
+import { BuildStep_FSFormat } from 'tools/lib/steps/FS-Format.js';
 
 const builder = new Builder(Bun.argv[2] === '--watch' ? 'watch' : 'build');
 
 builder.setStartupSteps([
-  BuildStep_BunInstall(),
-  BuildStep_IOFormat(),
+  BuildStep_BunInstall('quiet'),
+  BuildStep_FSFormat('quiet'),
   //
 ]);
 
