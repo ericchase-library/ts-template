482bad809758c0794016a605587f3cb4c624689e
ericchase <github@eric.addymail.com>
2025-03-15 22:59:33 -0500

    bug fixes
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -1,11 +1,13 @@
 import { Builder } from 'tools/lib/Builder.js';
 import { Step_Bun_Run } from 'tools/lib/steps/Bun-Run.js';
+import { Step_CleanDirectory } from 'tools/lib/steps/FS-CleanDirectory.js';
 import { Step_Format } from 'tools/lib/steps/FS-Format.js';
 
 const builder = new Builder(Bun.argv[2] === '--watch' ? 'watch' : 'build');
 
 builder.setStartupSteps([
   Step_Bun_Run({ cmd: ['bun', 'install'] }),
+  Step_CleanDirectory(builder.dir.out),
   Step_Format(),
   //
 ]);
