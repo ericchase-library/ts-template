5fb658e05da80291a1a305a17dde002daa07c181
ericchase <github@eric.addymail.com>
2025-03-18 18:14:09 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -7,21 +7,30 @@ import { Step_Format } from 'tools/lib/steps/FS-Format.js';
 // Use command line arguments to set watch mode.
 const builder = new Builder(Bun.argv[2] === '--watch' ? 'watch' : 'build');
 
-// During "dev" mode (when "--watch" is passed as an argument), the dev server
-// will start running with hot refreshing if enabled in your index file.
+// These steps are run during the startup phase only.
 builder.setStartupSteps([
-  Step_Bun_Run({ cmd: ['bun', 'install'] }),
+  Step_Bun_Run({ cmd: ['bun', 'install'] }, 'quiet'),
   Step_CleanDirectory(builder.dir.out),
-  Step_Format(),
+  Step_Format('quiet'),
   //
 ]);
 
+// These steps are run before each processing phase.
 builder.setBeforeProcessingSteps([]);
+
+// The processors are run for every file that added them during every
+// processing phase.
 builder.setProcessorModules([]);
+
+// These steps are run after each processing phase.
 builder.setAfterProcessingSteps([
+  // During "dev" mode (when "--watch" is passed as an argument), the server
+  // will start running with hot refreshing if enabled in your index file.
   Step_StartServer(),
   //
 ]);
+
+// These steps are run during the shutdown phase only.
 builder.setCleanupSteps([]);
 
 await builder.start();
