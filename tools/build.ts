29ac3851619bf0cce599ebb277dc70d8fd0192be
ericchase <github@eric.addymail.com>
2025-03-24 22:34:28 -0500

    remove spaces from file paths
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -8,31 +8,31 @@ import { Step_Format } from './lib/steps/FS-Format.js';
 const builder = new Builder(Bun.argv[2] === '--watch' ? 'watch' : 'build');
 
 // These steps are run during the startup phase only.
-builder.setStartupSteps([
+builder.setStartupSteps(
   Step_Bun_Run({ cmd: ['bun', 'install'] }, 'quiet'),
   Step_CleanDirectory(builder.dir.out),
   Step_Format('quiet'),
   //
-]);
+);
 
 // These steps are run before each processing phase, only if there are
 // processors to run.
-builder.setBeforeProcessingSteps([]);
+builder.setBeforeProcessingSteps();
 
 // The processors are run for every file that added them during every
 // processing phase.
-builder.setProcessorModules([]);
+builder.setProcessorModules();
 
 // These steps are run after each processing phase, only if there are
 // processors to run.
-builder.setAfterProcessingSteps([
+builder.setAfterProcessingSteps(
   // During "dev" mode (when "--watch" is passed as an argument), the server
   // will start running with hot refreshing if enabled in your index file.
   Step_StartServer(),
   //
-]);
+);
 
 // These steps are run during the shutdown phase only.
-builder.setCleanupSteps([]);
+builder.setCleanupSteps();
 
 await builder.start();
