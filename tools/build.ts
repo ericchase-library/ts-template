f592a224643773902f47433841c5b9b51942472d
ericchase <github@eric.addymail.com>
2025-08-03 02:51:37 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -1,9 +1,9 @@
 import { BunPlatform_Args_Has } from '../src/lib/ericchase/BunPlatform_Args_Has.js';
+import { Step_Dev_Format } from './core-dev/step/Step_Dev_Format.js';
+import { Step_Dev_Server } from './core-web/step/Step_Dev_Server.js';
 import { Builder } from './core/Builder.js';
 import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 import { Step_FS_Clean_Directory } from './core/step/Step_FS_Clean_Directory.js';
-import { Step_Dev_Format } from './lib-dev/step/Step_Dev_Format.js';
-import { Step_Dev_Server } from './lib-web/step/Step_Dev_Server.js';
 
 // Use command line arguments to set dev mode.
 if (BunPlatform_Args_Has('--dev')) {
