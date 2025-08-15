852dde94d81308723a205dea19989f8036de9835
ericchase <github@eric.addymail.com>
2025-08-15 07:22:33 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/pull-lib.ts
+++ tools/pull-lib.ts
@@ -1,14 +1,17 @@
-import { Step_Dev_Project_Sync_Server } from './core-dev/step/Step_Dev_Project_Sync_Server.js';
 import { Builder } from './core/Builder.js';
 import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 
-// This script pulls dev server files from template project.
+// This script pulls template lib files from template project.
 
 const template_path = 'C:/Code/Base/JavaScript-TypeScript/@Template';
 
 Builder.SetStartUpSteps(
   Step_Bun_Run({ cmd: ['bun', 'install'], showlogs: false }),
-  Step_Dev_Project_Sync_Server({ from_path: template_path, to_path: './' }),
+  // Step_FS_Mirror_Directory({
+  //   from_path: NODE_PATH.join(template_path, 'tools/lib-'),
+  //   to_path: NODE_PATH.join(Builder.Dir.Tools, 'lib-'),
+  //   include_patterns: ['**/*'],
+  // }),
   //
 );
 
